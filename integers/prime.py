371cde0c26e45194d1d30aa418ecb512428d2d56
blackbat13 <blackbat13@gmail.com>
2017-06-22 16:37:57 +0200

    Added prime number test.

---
+++ integers/prime.py
@@ -0,0 +1,19 @@
+import math
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+
+    return True
+
+
+n = int(input('Enter number'))
+if is_prime(n):
+    print(f'{n} is a prime number')
+else:
+    print(f'{n} is not a prime number')
