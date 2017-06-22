5c89c6cb50e8057aaac696035d1f66f474503ffa
blackbat13 <blackbat13@gmail.com>
2017-06-22 17:45:55 +0200

    Added erastosthenes sieve

---
+++ integers/erastosthenes_sieve.py
@@ -0,0 +1,24 @@
+def sieve(n):
+    prime = [False, False]
+    for i in range(2, n + 1):
+        prime.append(True)
+
+    for i in range(2, n):
+        if not prime[i]:
+            continue
+
+        for j in range(2 * i, n + 1, i):
+            prime[j] = False
+
+    return prime
+
+
+def print_prime_numbers(prime):
+    for i in range(0, len(prime)):
+        if prime[i]:
+            print(i)
+
+
+n = int(input('Enter number'))
+prime = sieve(n)
+print_prime_numbers(prime)
