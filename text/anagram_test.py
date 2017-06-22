7ba2cb6449cc771022d1bbb82e2527a8bd6ae765
blackbat13 <blackbat13@gmail.com>
2017-06-22 18:38:08 +0200

    Added anagrams check

---
+++ text/anagram_test.py
@@ -0,0 +1,12 @@
+def are_anagrams(a, b):
+    a = sorted(a)
+    b = sorted(b)
+    return a == b
+
+
+a = input('Enter first string')
+b = input('Enter second string')
+if are_anagrams(a, b):
+    print(f'{a} and {b} are anagrams')
+else:
+    print(f'{a} and {b} are not anagrams')
