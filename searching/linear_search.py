5f09378ddfe040f3f0c6fe2b1d9864782de2f735
blackbat13 <blackbat13@gmail.com>
2017-06-22 18:21:21 +0200

    Added linear search

---
+++ searching/linear_search.py
@@ -0,0 +1,15 @@
+def linear_search(array, number):
+    for i in range(0, len(array)):
+        if number == array[i]:
+            return i
+
+    return -1
+
+
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+number = int(input('Enter number to find'))
+index = linear_search(array, number)
+if index == -1:
+    print('Number not found')
+else:
+    print(f'Number position is {index}')
