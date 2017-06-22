b864af0de9798ceea5ab27f2470f24a9ed5dfdd8
blackbat13 <blackbat13@gmail.com>
2017-06-22 18:41:53 +0200

    Added palindrome check

---
+++ text/palindrome.py
@@ -0,0 +1,10 @@
+def is_palindrome(a):
+    b = ''.join(reversed(a))
+    return a == b
+
+
+a = input('Enter string')
+if is_palindrome(a):
+    print(f'{a} is a palindrome')
+else:
+    print(f'{a} is not a palindrome')
