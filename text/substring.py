d5fa1f884232164566685c576a8b269d756a594e
blackbat13 <blackbat13@gmail.com>
2017-06-22 23:30:00 +0200

    Added substring

---
+++ text/substring.py
@@ -0,0 +1,20 @@
+def substring_pos(a, b):
+    for i in range(0, len(a) - len(b)):
+        j = 0
+        while j < len(b) and b[j] == a[i + j]:
+            j += 1
+
+        if j == len(b):
+            return i
+
+    return -1
+
+
+a = input('Enter string to search in')
+b = input('Enter string to search for')
+
+pos = substring_pos(a, b)
+if pos == -1:
+    print(f'{b} is not a substring of {a}')
+else:
+    print(f'{b} is substring of {a} and starts at position {pos}')
