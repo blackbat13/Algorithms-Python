c05bdc9b0cf1aa3e68c65aa6c16fc2f393644015
blackbat13 <blackbat13@gmail.com>
2017-07-03 18:40:29 +0200

    Added subtraction gcd

--- integers/gcd.py
+++ integers/gcd.py
@@ -13,8 +13,22 @@ def gcd_recursive(a, b):
     return gcd_recursive(b, a % b)
 
 
+def subtraction_gcd(a, b):
+    while a > 0 and b > 0:
+        if a > b:
+            a -= b
+        else:
+            b -= a
+
+    if a == 0:
+        return b
+    else:
+        return a
+
+
 a = int(input('Enter a'))
 b = int(input('Enter b'))
 
 print(f'Iterative GCD({a},{b}) = {gcd_iterative(a, b)}')
 print(f'Recursive GCD({a},{b}) = {gcd_recursive(a, b)}')
+print(f'Subtraction GCD({a},{b}) = {subtraction_gcd(a, b)}')
