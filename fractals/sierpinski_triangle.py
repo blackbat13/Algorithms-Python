e49dac50d6789be0761b759344c8ea746bcc1269
blackbat13 <blackbat13@gmail.com>
2017-07-06 19:25:47 +0200

    Added sierpinski triangle drawing

---
+++ fractals/sierpinski_triangle.py
@@ -0,0 +1,25 @@
+import turtle
+import math
+
+
+def sierpinski_triangle(rank, length):
+    if rank == 0:
+        turtle.begin_fill()
+        for _ in range(0, 3):
+            turtle.forward(length)
+            turtle.left(120)
+        turtle.end_fill()
+        return
+
+    for _ in range(0, 3):
+        sierpinski_triangle(rank - 1, math.floor(length / 2))
+        turtle.forward(length)
+        turtle.left(120)
+
+
+turtle.speed(0)
+turtle.penup()
+turtle.back(200)
+turtle.pendown()
+sierpinski_triangle(4, 300)
+turtle.done()
