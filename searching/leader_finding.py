d34363442ed6c9d2fa7e0eac8c29a8902820a97a
blackbat13 <blackbat13@gmail.com>
2017-07-06 20:23:59 +0200

    Modified leader finding algorithm

--- searching/leader_finding.py
+++ searching/leader_finding.py
@@ -1,11 +1,11 @@
 def find_leader(array):
     counter = 0
-    for i in range(0, len(array)):
+    for el in array:
         if counter == 0:
-            current_candidate = array[i]
+            current_candidate = el
             counter = 1
         else:
-            if array[i] == current_candidate:
+            if el == current_candidate:
                 counter += 1
             else:
                 counter -= 1
@@ -13,6 +13,17 @@ def find_leader(array):
     return current_candidate
 
 
+def count_occurrences(element, array):
+    count = 0
+    for el in array:
+        if el == element:
+            count += 1
+
+    return count
+
 array = [1, 2, 5, 5, 7, 5, 5, 10, 5, 5]
 majority = find_leader(array)
-print(f'Majority element is: {majority}')
+if count_occurrences(majority, array) >= len(array) / 2:
+    print(f'Majority element is: {majority}')
+else:
+    print('There is no majority element in given array')
