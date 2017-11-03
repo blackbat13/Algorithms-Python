ea82d6f39a7bb0c70c7ee6df5ba43426211977d8
blackbat <blackbat13@gmail.com>
2017-11-03 18:46:58 +0100

    Added introduction to arrays

---
+++ basics/arrays.py
@@ -0,0 +1,28 @@
+print('Creating an empty array')
+array = []
+print(array)
+
+print()
+
+print('Creating array populated with some data')
+array = [2, 4, 6, 8]
+print(array)
+
+print()
+
+print('Adding single elements to the array')
+array.append(10)
+array.append(12)
+print(array)
+
+print()
+
+print('Creating array containing 10 elements')
+print('Yeah... we need to use some kind of loop')
+array = []
+for i in range(0, 10):
+    array.append(i)
+
+print(array)
+
+print()
