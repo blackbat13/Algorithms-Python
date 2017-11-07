f616bb9d37ec589670cf7b52edcb870ac4b3991c
blackbat <blackbat13@gmail.com>
2017-11-07 11:55:06 +0100

    Added drawing fibonacci and prime numbers

---
+++ en/fractals/fibonacci_numbers.py
@@ -0,0 +1,17 @@
+import turtle
+
+
+def draw_fibonacci(n):
+    f1 = 1
+    f2 = 1
+    turtle.right(90)
+    for i in range(0, n):
+        turtle.forward(f1)
+        turtle.left(90)
+        f3 = f1 + f2
+        f1 = f2
+        f2 = f3
+
+
+draw_fibonacci(15)
+turtle.done()
