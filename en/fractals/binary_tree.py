ead56c0057f1a9a90204588e4ab6b6506799cf47
blackbat <blackbat13@gmail.com>
2018-06-05 20:31:55 +0200

    Added binary tree fractal

---
+++ en/fractals/binary_tree.py
@@ -0,0 +1,22 @@
+import turtle
+
+
+def binary_tree(rank, length):
+    turtle.forward(length)
+    if rank > 0:
+        turtle.left(45)
+        binary_tree(rank - 1, length / 2)
+        turtle.right(90)
+        binary_tree(rank - 1, length / 2)
+        turtle.left(45)
+    turtle.back(length)
+
+
+turtle.speed(0)
+turtle.penup()
+turtle.left(90)
+turtle.back(350)
+turtle.pendown()
+turtle.pensize(3)
+binary_tree(5, 400)
+turtle.done()
