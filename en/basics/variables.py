cf957781716cb5fa7201f67f4cc1aba5abf6abd9
blackbat13 <blackbat13@gmail.com>
2018-06-16 12:39:00 +0200

    Added part of introduction to variables

---
+++ en/basics/variables.py
@@ -0,0 +1,26 @@
+print("Variables in Python are very easy to use. Just choose a name and give it some value!")
+
+x = 20
+
+print(f"x = {x}")
+
+print("In Python we don't have type control as in C++")
+print("Type of variable is computed automatically")
+print("That means that you can use your variable as number at first, then assign it text value and etc.")
+
+x = "Some text"
+
+print(f"x = {x}")
+
+print("Although you shouldn't do that! It's easy to loose track of what is happening in your code.")
+print("Good programmers maintain their types")
+
+print("That said we have several types of variables in Python.")
+print("Let me introduce you to some of them.")
+
+print("First we have numbers.")
+print("Integers:", 23)
+print("And real numbers:", 23.9)
+
+print("We also have strings (text), obviously:", "Some text")
+print("And boolean type:", True, "and", False)
