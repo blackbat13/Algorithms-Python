c1c405d6e6b22a6fce522bbd487b40e96d1e97a7
blackbat13 <blackbat13@gmail.com>
2018-06-16 13:37:15 +0200

    Added peano curve

---
+++ en/fractals/peano_curve.py
@@ -0,0 +1,23 @@
+import turtle
+
+
+def peano_curve(rank, angle, length):
+    if rank > 0:
+        turtle.right(angle)
+        peano_curve(rank - 1, -angle, length)
+        turtle.forward(length)
+        peano_curve(rank - 1, angle, length)
+        turtle.forward(length)
+        peano_curve(rank - 1, -angle, length)
+        turtle.left(angle)
+
+
+turtle.speed(0)
+turtle.penup()
+turtle.back(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.right(90)
+turtle.pendown()
+peano_curve(4, 90, 20)
+turtle.done()
