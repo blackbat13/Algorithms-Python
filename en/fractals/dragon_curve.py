29108b18396684b6780aca01e79d49763ee6a198
blackbat13 <blackbat13@gmail.com>
2018-06-16 13:47:08 +0200

    Added dragon curve

---
+++ en/fractals/dragon_curve.py
@@ -0,0 +1,23 @@
+import turtle
+
+
+def dragon_curve(rank, sign, length):
+    if rank > 0:
+        turtle.left(45 * sign)
+        dragon_curve(rank - 1, 1, length)
+        turtle.right(90 * sign * -1)
+        dragon_curve(rank - 1, -1, length)
+        turtle.left(45 * sign)
+    else:
+        turtle.forward(length)
+
+
+turtle.speed(0)
+turtle.penup()
+turtle.back(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.right(90)
+turtle.pendown()
+dragon_curve(10, 1, 5)
+turtle.done()
