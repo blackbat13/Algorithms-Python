832bc2b7c8b8464bd00afcf8774660e39f2df8e8
blackbat13 <blackbat13@gmail.com>
2019-09-09 15:36:37 +0200

    Added Vigenere cipher

---
+++ en/cryptography/vigenere_cipher.py
@@ -0,0 +1,49 @@
+def is_letter(letter):
+    return ord('a') <= ord(letter) <= ord('z')
+
+
+def encode(message:str, key:str):
+    encoded = ''
+    key_index = 0
+    for i in range(0, len(message)):
+        if not is_letter(message[i]):
+            encoded += message[i]
+            continue
+        k = ord(key[key_index]) - ord('a')
+        letter = ord(message[i]) + k
+        if letter > ord('z'):
+            letter = ord('a') + letter - ord('z')
+
+        letter = chr(letter)
+        encoded += letter
+        key_index += 1
+        key_index %= len(key)
+
+    return encoded
+
+
+def decode(message:str, key:str):
+    decoded = ''
+    key_index = 0
+    for i in range(0, len(message)):
+        if not is_letter(message[i]):
+            decoded += message[i]
+            continue
+        k = ord(key[key_index]) - ord('a')
+        letter = ord(message[i]) - k
+        if letter < ord('a'):
+            letter = ord('z') - (ord('a') - letter)
+
+        letter = chr(letter)
+        decoded += letter
+        key_index += 1
+        key_index %= len(key)
+
+    return decoded
+
+
+message = 'computer science'
+encoded = encode(message, 'cat')
+decoded = decode(encoded, 'cat')
+print(f'Encoded: {encoded}')
+print(f'Decoded: {decoded}')
