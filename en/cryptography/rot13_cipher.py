e90a6de57d90169c12dc020545c5cd8510e4326b
blackbat13 <blackbat13@gmail.com>
2019-09-09 15:28:32 +0200

    Added rot13 cipher

---
+++ en/cryptography/rot13_cipher.py
@@ -0,0 +1,41 @@
+def is_letter(letter):
+    return ord('a') <= ord(letter) <= ord('z')
+
+
+def encode(message):
+    encoded = ''
+    for i in range(0, len(message)):
+        if not is_letter(message[i]):
+            encoded += message[i]
+            continue
+        letter = ord(message[i]) + 13
+        if letter > ord('z'):
+            letter = ord('a') + letter - ord('z')
+
+        letter = chr(letter)
+        encoded += letter
+
+    return encoded
+
+
+def decode(message):
+    decoded = ''
+    for i in range(0, len(message)):
+        if not is_letter(message[i]):
+            decoded += message[i]
+            continue
+        letter = ord(message[i]) - 13
+        if letter < ord('a'):
+            letter = ord('z') - (ord('a') - letter)
+
+        letter = chr(letter)
+        decoded += letter
+
+    return decoded
+
+
+message = 'computer science'
+encoded = encode(message)
+decoded = decode(encoded)
+print(f'Encoded: {encoded}')
+print(f'Decoded: {decoded}')
