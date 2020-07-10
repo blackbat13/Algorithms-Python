cd9cc10bfef38a34fb9f2a698c2a52c19da7a583
blackbat13 <blackbat13@gmail.com>
2020-07-10 14:22:31 +0200

    Removed english version

--- en/matrix/det.py
+++
@@ -1,7 +0,0 @@
-def det3(matrix) -> int:
-    return matrix[0][0] * matrix[1][1] * matrix[2][2] + matrix[1][0] * matrix[2][1] * matrix[0][2] + matrix[2][0] * \
-           matrix[0][1] * matrix[1][2] - matrix[0][2] * matrix[1][1] * matrix[2][0] - matrix[0][1] * matrix[1][0] * \
-           matrix[2][2] - matrix[0][0] * matrix[1][2] * matrix[2][1]
-
-
-print(det3([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
--- en/text/palindrome.py
+++
@@ -1,9 +0,0 @@
-def is_palindrome(a: str) -> bool:
-    return a == ''.join(reversed(a))
-
-
-a: str = input('Enter string: ')
-if is_palindrome(a):
-    print(f'{a} is a palindrome')
-else:
-    print(f'{a} is not a palindrome')
