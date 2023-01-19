7ffedf8df7bbcaeed4b43df3c78b94128c3ff02f
Damian Kurpiewski <blackbat13@gmail.com>
2023-01-19 15:28:43 +0100

    Added square root plot

--- numerical/square_root_plot.py
+++ numerical/square_root_plot.py
@@ -1,9 +1,17 @@
+import matplotlib.pyplot as plt
+
+
 def sqrt(n: float, p: float) -> float:
     x1 = n / 2
     x2 = (x1 + (n / x1)) / 2
+    plot_values = [x1, x2]
     while abs(x2 - x1) > p:
         x1 = (x2 + n / x2) / 2
         x1, x2 = x2, x1
+        plot_values.append(x2)
+
+    plt.plot(plot_values, label=f"heron({n},{p})")
+    plt.legend()
 
     return x2
 
@@ -14,3 +22,5 @@ p = 0.00000001
 result = sqrt(n, p)
 
 print(f"sqrt({n}) ~= {result}")
+
+plt.show()
